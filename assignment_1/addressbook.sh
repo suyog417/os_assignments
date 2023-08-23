79bd201ea584fca32657e1f634100dc9fe953eef
suyog212 <suyog.22220300@viit.ac.in>
2023-08-23 08:37:36 +0530

    first commit

--- assignment_1/addressbook.sh
+++ assignment_1/addressbook.sh
@@ -7,14 +7,24 @@ echo "3. Insert a record"
 echo "4. Delete a record"
 echo "5. Modify a record"
 echo "6. Exit"
+read -p "Enter your choice" choice
 
-read -p "Enter the name for your Addressbook : " fname
-touch "$fname.txt"
 
-read -p "Enter the name : " name
-read -p "Enter the age : " age
-read -p "Enter the phone : " phone
-echo "$name\t$age\t$phone" >> "abc.txt"
 
+case $choice in 
+    1 )
+        read -p "Enter the name for your Addressbook : " fname
+        touch $fname.txt
+    ;;
+    2 ) 
+        echo "Name\tAge\tPhone"
+        cat $fname.txt
+    ;;
+    3 )
+        read -p "Enter the name : " name
+        read -p "Enter the age : " age
+        read -p "Enter the phone : " phone
+        echo "$name\t$age\t$phone" >> "abc.txt"
+esac
 
 ./view.sh $fname.txt
--- assignment_1/view.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Name\tAge\tPhone"
-cat $1
