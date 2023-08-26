73ea5793132121f2be581ed3c055b4cf39529cdd
suyog212 <suyog.22220300@viit.ac.in>
2023-08-26 17:52:25 +0530

    Done

--- assignment_1/addressbook.sh
+++ assignment_1/addressbook.sh
@@ -1,30 +1,50 @@
 #!/bin/sh
 
+read -p "Enter the name of addressbook to create or open it : " fname
+touch "$fname.csv"
 
-echo "1. Create a Addressbook"
-echo "2. View Addressbook"
-echo "3. Insert a record"
-echo "4. Delete a record"
-echo "5. Modify a record"
-echo "6. Exit"
-read -p "Enter your choice" choice
-
-
-
-case $choice in 
-    1 )
-        read -p "Enter the name for your Addressbook : " fname
-        touch $fname.txt
-    ;;
-    2 ) 
-        echo "Name\tAge\tPhone"
-        cat $fname.txt
-    ;;
-    3 )
-        read -p "Enter the name : " name
-        read -p "Enter the age : " age
-        read -p "Enter the phone : " phone
-        echo "$name\t$age\t$phone" >> "abc.txt"
-esac
-
-./view.sh $fname.txt
+cnt=0
+while [ cnt==0 ]
+do
+    echo "1.Insert record"
+    echo "2.Modify record"
+    echo "3.Delete record"
+    echo "4.View Addressbook"
+    echo "5.Exit"
+    read -p "Enter your choice : " ch
+    case $ch in 
+        1 ) 
+            read -p "Enter name : " name
+            read -p "Enter age : " age
+            read -p "Enter phone : " phone
+            read -p "Enter city : " city
+            echo "$name\t$age\t$phone\t$city" >> "$fname.csv"
+        ;;
+        2 )
+            cat "$fname.csv"
+            read -p "Enter line number for the record you want to edit " line_no
+            echo "Enter new data :  "
+            read -p "Enter name : " name
+            read -p "Enter age : " age
+            read -p "Enter phone : " phone
+            read -p "Enter city : " city
+            sed -i "${line_no}s/.*/"${name}"\t"${age}"\t"${phone}"\t"${city}"/" "$fname.csv"
+            echo "Record Modified"
+        ;;
+        3 )
+            read -p "Enter the line number of the record you want to delete : " line_no
+            sed -i "${line_no}d" "$fname.csv"
+            echo "Record Deleted."
+        ;;
+        4 )
+            echo "Name\tAge\tPhone\tCity"
+            cat "$fname.csv"
+        ;;
+        5 ) 
+            exit
+        ;;
+        * )
+            echo "Invaid Option.Enter a valid number from the list above."
+        ;;
+    esac
+done
