4d3a1e1bf7f2960d16d6908ff6f1232b0c8673c1
suyog212 <suyog.22220300@viit.ac.in>
2023-08-27 07:32:38 +0530

    Done

--- addressbook.sh
+++ addressbook.sh
@@ -6,12 +6,9 @@ touch "$fname.csv"
 cnt=0
 while [ cnt==0 ]
 do
-    echo "1.Insert record"
-    echo "2.Modify record"
-    echo "3.Delete record"
-    echo "4.View Addressbook"
-    echo "5.Exit"
+    echo "1.Insert record\n2.Modify record\n3.Delete record\n4.View Addressbook\n5.Exit"
     read -p "Enter your choice : " ch
+    echo " \r "
     case $ch in 
         1 ) 
             read -p "Enter name : " name
@@ -21,7 +18,8 @@ do
             echo "$name\t$age\t$phone\t$city" >> "$fname.csv"
         ;;
         2 )
-            cat "$fname.csv"
+            echo "Sr.No\tName\tAge\tPhone\tCity"
+            cat -n "$fname.csv"
             read -p "Enter line number for the record you want to edit " line_no
             echo "Enter new data :  "
             read -p "Enter name : " name
@@ -37,8 +35,9 @@ do
             echo "Record Deleted."
         ;;
         4 )
-            echo "Name\tAge\tPhone\tCity"
-            cat "$fname.csv"
+            echo "Sr.No\tName\tAge\tPhone\tCity"
+            cat -n "$fname.csv"
+            echo " \n"
         ;;
         5 ) 
             exit
